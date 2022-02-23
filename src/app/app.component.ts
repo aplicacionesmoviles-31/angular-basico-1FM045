bf52bc221f128b2d4898abd2427debf0f030331b
Luz <aracely.des@uanl.edu.mx>
2022-02-22 19:57:43 -0600

    Inicializando proyecto de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-basico';
+  title = 'Hola, mundo!';
+
+  imagenes = [
+    "../assets/imagenes/cheems.jpeg",
+    "../assets/imagenes/amsiedad.webp",
+    "../assets/imagenes/meme-perrillo.jpeg"
+  ];
+
+  ingles = false;
 }
