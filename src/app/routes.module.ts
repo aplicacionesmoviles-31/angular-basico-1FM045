9851671253e398d2588a04b9b713e1ed94aac249
Luz <aracely.des@uanl.edu.mx>
2022-03-08 20:45:06 -0600

    avance rutas

--- src/app/routes.module.ts
+++ src/app/routes.module.ts
@@ -5,10 +5,25 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import  { FeedComponent } from './feed/feed.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { TabsComponent } from './tabs/tabs.component';
+
+
+import { PerfilModule } from './perfil/perfil.module';
 
 const routes: Routes = [
-  { path: 'feed', component: FeedComponent },
-  { path: 'perfil', component: PerfilComponent }
+  /*{ path: 'feed', component: FeedComponent },
+  { path: 'perfil', component: PerfilComponent }*/
+  {
+    path: 'perfil', 
+    component: TabsComponent, 
+    children: [
+    {
+      path: '', 
+      loadChildren: () => import('../tabs/tabs.module').then(m => m.TabsPageModule)'../tabs/tabs.module' 
+    
+    }
+  ]
+}
 ]
 
 @NgModule({
